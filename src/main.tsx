cbe03e2b25cbfcc6f127d5310380c35c9b95aff4
ADNY <66500121+ErKeLost@users.noreply.github.com>
2023-12-05 05:35:05 +0000

    Update 6 files

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,6 @@ export function Main() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Farm + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
