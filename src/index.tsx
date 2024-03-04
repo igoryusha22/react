11ca459a18083af7f28029ba38e1cd5e1504963b
Igor <56885648+igoryusha22@users.noreply.github.com>
2024-03-04 10:07:49 +0000

    [ADD] commit check.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import {Provider} from 'react-redux';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Main } from './main';
-import { store } from './store';
-import './index.css'
+import { Provider } from "react-redux";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Main } from "./main";
+import { store } from "./store";
 
-const container = document.querySelector('#root');
+import "./index.css";
+
+const container = document.querySelector("#root");
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
-        <Main />
-    </Provider>
+  <Provider store={store}>
+    <Main />
+  </Provider>,
 );
